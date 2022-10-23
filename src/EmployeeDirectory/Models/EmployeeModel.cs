15b30e7b237de1c6bc3f6b3e420a9c160fc75731
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2022-10-23 17:18:16 +0200

    step01

---
+++ src/EmployeeDirectory/Models/EmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeDirectory.Models
+{
+    public class EmployeeModel
+    {
+        public Guid ID { get; set; }
+        public DateTime Joined { get; set; }
+        public string Name { get; set; } = "";
+        public string Image { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string JotTitle { get; set; } = "";
+        public string Department { get; set; } = "";
+    }
+}
---
+++ src/EmployeeDirectory/Services/EmployeeService.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using EmployeeDirectory.Models;
+
+namespace EmployeeDirectory.Services
+{
+    public class EmployeeService
+    {
+        public async Task<EmployeeModel[]> GetEmployeeAsync()
+        {
+            var userFaker = new Faker<EmployeeModel>()
+                .RuleFor(o => o.ID, f => f.Random.Uuid())
+                .RuleFor(o => o.Joined, f => f.Date.Recent(100))
+                .RuleFor(o => o.Name, f => $"{f.Name.FirstName()} {f.Name.LastName()}")
+                .RuleFor(o => o.Image, f => f.Internet.Avatar())
+                .RuleFor(o => o.Email, (f, u) => f.Internet.Email())
+                .RuleFor(o => o.JotTitle, f => f.Name.JobTitle())
+                .RuleFor(o => o.Department, f => f.Name.JobArea());
+
+            var employees = userFaker.Generate(99);
+
+            //await Task.Delay(3000);
+
+            return employees.ToArray();
+        }
+    }
+}
